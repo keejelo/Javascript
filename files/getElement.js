827fae6217c23050da80359c007381fae9d73e22
keejelo <34620895+keejelo@users.noreply.github.com>
2021-03-23 18:53:36 +0100

    Update getElement.js

--- files/getElement.js
+++ files/getElement.js
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------------------------------
 // GetElement - combined shorthand for document.querySelector() and document.querySelectorAll()
-// Version: 1.10
+// Version: 0.1
 // Created by: keejelo
 // Year: 2010
 //----------------------------------------------------------------------------------------------------
@@ -23,27 +23,9 @@
 // Just be sure that if using $() then you cannot use jQuery $() , or any other library that uses that
 // character as a function or element selection. :)
 //----------------------------------------------------------------------------------------------------
-var getEl = function(s,o)
+var getEl = function(s, pNode)
 {
-    // Trim leading and trailing spaces from selector string before we use it
-    s = s.replace(/^\s+|\s+$/g, '');
-
-    // If selector string contains a space or comma, then we know there are more elements in string
-    if(s.indexOf(' ') != -1 || s.indexOf(',') != -1)
-    {
-        if((o||document).querySelectorAll(s).length > 0)
-        {
-            return (o||document).querySelectorAll(s);
-        }
-    }
-    else
-    {
-        // Only one element in string
-        return (o||document).querySelector(s);
-    }
-
-    // No matching element selection found
-    return null;
+    return (pNode||document).querySelectorAll(s);
 };
 //----------------------------------------------------------------------------------------------------
 // END: GetElement
