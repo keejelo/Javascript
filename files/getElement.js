b35cf50cacd0878684dde80ee23c87d8322635d4
keejelo <34620895+keejelo@users.noreply.github.com>
2021-03-23 18:38:47 +0100

    Update getElement.js

--- files/getElement.js
+++ files/getElement.js
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------------------------------
 // GetElement - combined shorthand for document.querySelector() and document.querySelectorAll()
-// Version: 1.09
+// Version: 1.10
 // Created by: keejelo
 // Year: 2010
 //----------------------------------------------------------------------------------------------------
@@ -28,36 +28,19 @@ var getEl = function(s,o)
     // Trim leading and trailing spaces from selector string before we use it
     s = s.replace(/^\s+|\s+$/g, '');
 
-    // If string starts with # (hash) id
-    if(s.indexOf('#') != -1)
+    // If selector string contains a space or comma, then we know there are more elements in string
+    if(s.indexOf(' ') != -1 || s.indexOf(',') != -1)
     {
-        // If selector string contains a space then we know there are more elements
-        if(s.indexOf(' ') != -1)
-        {                                     // The below assumes that string starts with hash '#'
-            var id = s.split(' ')[0];         // Get first part of string before first space ' '
-            s = s.replace(id, '');            // Get second half of string by removing '#id'
-            s = s.replace(/^\s+|\s+$/g, '');  // Trim string of leading and trailing spaces (again)
-
-            // Check if element exist in DOM
-            if((o||document).querySelector(id).querySelectorAll(s).length > 0)
-            {
-                return (o||document).querySelector(id).querySelectorAll(s);
-            }
-        }
-        else
-        {
-            // Since an id should be unique, there should be only one element with that id
-            return (o||document).querySelector(s);
-        }
-    }
-    else
-    {
-        // Since string did NOT start with # (hash) id, we search for native DOM elements
         if((o||document).querySelectorAll(s).length > 0)
         {
             return (o||document).querySelectorAll(s);
         }
-    };
+    }
+    else
+    {
+        // Only one element in string
+        return (o||document).querySelector(s);
+    }
 
     // No matching element selection found
     return null;
