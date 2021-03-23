6d1c07537a268fdac5c8d8e7513ba8adcade213a
keejelo <34620895+keejelo@users.noreply.github.com>
2021-03-23 16:12:43 +0100

    Update getElement.js

--- files/getElement.js
+++ files/getElement.js
@@ -1,56 +1,63 @@
-/*****************************************************************************************************
-* GetElements - combined shorthand for .querySelector() and .querySelectorAll()
-* Version: 1.0
-* Created by: keejelo
-* Year: 2020-2021
-*****************************************************************************************************/
+//----------------------------------------------------------------------------------------------------
+// GetElement - combined shorthand for .querySelector() and .querySelectorAll()
+// Version: 1.1
+// Created by: keejelo
+// Year: 2020-2021
+//----------------------------------------------------------------------------------------------------
+//
+// Examples on how to use:
+//
+// var el = getEl('#myDiv');
+// var el = getEl('div p.myClass');
+// var el = getEl('div p span');
+//
+// etc.
+//
+//----------------------------------------------------------------------------------------------------
 
 
 //----------------------------------------------------------------------------------------------------
-// ** GetElements
+// GetElement
 //----------------------------------------------------------------------------------------------------
-var getEl = function(str)
+var getEl = function(s)
 {
-    // ** If string starts with # (hash) id
-    if(str.indexOf('#') != -1)
+    // If string starts with # (hash) id
+    if(s.indexOf('#') != -1)
     {        
-        // ** Trim leading and trailing spaces from string before we use it
-        str = str.replace(/^\s+|\s+$/g, '');
+        // Trim leading and trailing spaces from string before we use it
+        s = s.replace(/^\s+|\s+$/g, '');
 
-        // ** If string contains a space then we know there are more elements
-        if(str.indexOf(' ') != -1)
-        {                                         // The below assumes that string starts with hash '#'
-            var idPart = str.split(' ')[0];       // Get first part of string before first space ' '
-            str = str.replace(idPart, '');        // Get second half of string by removing '#idPart'
-            str = str.replace(/^\s+|\s+$/g, '');  // Trim string of leading and trailing spaces (again)
+        // If string contains a space then we know there are more elements
+        if(s.indexOf(' ') != -1)
+        {                                     // The below assumes that string starts with hash '#'
+            var id = s.split(' ')[0];         // Get first part of string before first space ' '
+            s = s.replace(id, '');            // Get second half of string by removing '#id'
+            s = s.replace(/^\s+|\s+$/g, '');  // Trim string of leading and trailing spaces (again)
             
-            // ** Check if element exist in DOM, else return: null
-            if(document.querySelector(idPart).querySelectorAll(str).length > 0)
+            // Check if element exist in DOM, else return: null
+            if(document.querySelector(id).querySelectorAll(s).length > 0)
             {
-                return document.querySelector(idPart).querySelectorAll(str);
+                return document.querySelector(id).querySelectorAll(s);
             }
         }
         else
         {
-            // ** Get the only element containing the specified id (id's should be unique)
-            return document.querySelector(str);
+            // Get the only element containing the specified id (id's should be unique)
+            return document.querySelector(s);
         }
     }
     else
     {
-        // TODO: if parent is not document, then use element as parent:
-        // return element.querySelectorAll(str);
-        
-        // ** Since string do NOT start with # (hash) id, then we search for DOM elements
-        if(document.querySelectorAll(str).length > 0)
+        // Since string do NOT start with # (hash) id, then we search for DOM elements        
+        if(document.querySelectorAll(s).length > 0)
         {
-            return document.querySelectorAll(str);
+            return document.querySelectorAll(s);
         }
     };
     
-    // ** No matching element selection found
+    // No matching element selection found
     return null;
 };
 //----------------------------------------------------------------------------------------------------
-// ** END: GetElements
+// END: GetElement
 //----------------------------------------------------------------------------------------------------
