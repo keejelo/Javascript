2711a4ba0afd871360b64d7806fb42b337769137
keejelo <34620895+keejelo@users.noreply.github.com>
2021-03-24 07:12:48 +0100

    Update getElement.js

--- files/getElement.js
+++ files/getElement.js
@@ -1,14 +1,18 @@
 //----------------------------------------------------------------------------------------------------
-// GetElement - combined shorthand for .querySelectorAll()
+// GetElement - shorthand for .querySelectorAll()
 // Version: 0.1
 // Created by: keejelo
 // Year: 2010
 //----------------------------------------------------------------------------------------------------
-// Syntax: getEl(selectors, parentNode);  // <-- if parentNode not used then default is: document
+// Syntax: getEl(selectors, parent);  // <-- if parent not used then default is: document
 //----------------------------------------------------------------------------------------------------
-var getEl = function(s, pNode)
+var getEl = function(selectors, elem)
 {
-    return (pNode||document).querySelectorAll(s);
+    if((elem||document).querySelectorAll(selectors).length > 0)
+    {
+        return (elem||document).querySelectorAll(selectors);
+    }
+    return null;
 };
 //----------------------------------------------------------------------------------------------------
 // END: GetElement
