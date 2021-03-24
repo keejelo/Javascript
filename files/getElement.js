08d2e60fa8c05ffa765760c5287b922d4a12cfda
keejelo <34620895+keejelo@users.noreply.github.com>
2021-03-24 18:00:17 +0100

    Update getElement.js

--- files/getElement.js
+++ files/getElement.js
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------------------------------
 // GetElement - combined shorthand for .querySelector() and .querySelectorAll()
-// Version: 1.0
+// Version: 1.01
 // Created by: keejelo
 // Year: 2010
 //----------------------------------------------------------------------------------------------------
@@ -17,16 +17,13 @@
 //----------------------------------------------------------------------------------------------------
 var getEl = function(s, p)
 {
-    // ** Clean string before working with it, trim leading and trailing spaces
     s = s.replace(/^\s+|\s+$/g, '');
     
     var a = (p || document).querySelectorAll(s);
     var b = (p || document).querySelector(s);
 
-    // ** If string contains # (hash) element id
     if(s.indexOf('#') != -1)
     {
-        // ** If string contains a space or comma
         if(s.indexOf(' ') != -1 || s.indexOf(',') != -1)
         {
             if(a.length > 0)
