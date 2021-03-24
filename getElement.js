e7388dcaa8c360eff2d295569719f4e2d2512e18
keejelo <34620895+keejelo@users.noreply.github.com>
2021-03-24 18:52:54 +0100

    Update getElement.js

--- getElement.js
+++ getElement.js
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------------
-// GetElement - combined shorthand for .querySelector() and .querySelectorAll()
+// GetElement - combined shorthand for querySelector() and querySelectorAll()
 // Version: 1.01
 // Created by: keejelo
 // Year: 2021
