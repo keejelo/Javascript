a96e7488eb9fd7b1ba392e9881eab436def74775
keejelo <34620895+keejelo@users.noreply.github.com>
2021-03-24 18:01:36 +0100

    Update getElement.js

--- files/getElement.js
+++ files/getElement.js
@@ -13,10 +13,8 @@
 var getEl = function(s, p)
 {
     s = s.replace(/^\s+|\s+$/g, '');
-    
     var a = (p || document).querySelectorAll(s);
     var b = (p || document).querySelector(s);
-
     if(s.indexOf('#') != -1)
     {
         if(s.indexOf(' ') != -1 || s.indexOf(',') != -1)
@@ -38,7 +36,6 @@ var getEl = function(s, p)
             return a;
         }
     };
-    
     return null;
 };
 //----------------------------------------------------------------------------------------------------
