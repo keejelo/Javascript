c5325df5f9fa57dfbd4b4970d08fd4674c9cc1a3
keejelo <34620895+keejelo@users.noreply.github.com>
2021-04-04 14:16:50 +0200

    Create get_elements_like_jquery.js

---
+++ get_elements_like_jquery.js
@@ -0,0 +1,48 @@
+//-------------------------------------------------------------------------------------
+// Get elements similar to jQuery with dollarsigns ($ and $$)
+//
+// One dollar ($) returns one element, or the first node in a nodelist
+//
+// Two dollars ($$) returns a nodelist
+//
+//-------------------------------------------------------------------------------------
+// ** One dollar ($) returns one element, or the first node in a nodelist
+// ** Extend HTMLElement prototype
+HTMLElement.prototype.$ = function(selector)
+{
+    return this.querySelector(selector);
+};
+// ** Create function for when parent is: document
+var $ = function(selector)
+{
+    return document.querySelector(selector);
+};
+
+// ** Two dollars ($$) returns a nodelist
+// ** Extend HTMLElement prototype
+HTMLElement.prototype.$$ = function(selector)
+{
+    return this.querySelectorAll(selector);
+};
+// ** Create function for when parent is: document
+var $$ = function(selector)
+{
+    return document.querySelectorAll(selector);
+};
+//-------------------------------------------------------------------------------------
+
+/*
+// Example:
+
+// ** Do some stuff after pageload
+addEventListener('load', function()
+{
+    var div = $('div');
+    
+    var p = div.$('p');
+    
+    console.log(div);
+    console.log(p);
+});
+
+*/
