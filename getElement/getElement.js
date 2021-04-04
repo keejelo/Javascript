7186577a630ffbc56761b04adbdd6788cf47c580
keejelo <34620895+keejelo@users.noreply.github.com>
2021-04-04 14:54:49 +0200

    Update getElement.js

--- getElement/getElement.js
+++ getElement/getElement.js
@@ -61,7 +61,7 @@ if(typeof getElement === 'undefined')
 }
 
 /*
-// ** Uncomment this to use dollarsign as function reference
+// Uncomment this to use dollarsign as function reference
 if(typeof $ === 'undefined')
 {
     var $ = getEl;
