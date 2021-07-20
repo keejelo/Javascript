fc9d5b694c13495d25c4b8a70ce88a660d0876aa
keejelo <34620895+keejelo@users.noreply.github.com>
2021-07-20 17:07:50 +0200

    Create fade.js

---
+++ fade/fade.js
@@ -0,0 +1,22 @@
+//-------------------------------------------------
+// ** Fade in/out
+//-------------------------------------------------
+// Parameters:
+// @el : element
+// @dir : true = fade out | false = fade in
+// @speed : animation speed
+//-------------------------------------------------
+var fade = function(el, dir, speed)
+{
+    var n;
+    (dir) ? n=10 : n=0;
+    var t = setInterval(function()
+    {
+        (dir) ? n-- : n++;
+        el.style.opacity = n / 10;
+        (n >= 10) ? clearInterval(t) : null;
+    }, speed || 40);
+};
+//-------------------------------------------------
+// ** END: Fade in/out
+//-------------------------------------------------
