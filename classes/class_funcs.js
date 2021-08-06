3f56dc61a49d8378f32e9ec5321d2dd50d45032b
keejelo <34620895+keejelo@users.noreply.github.com>
2021-08-06 17:46:32 +0200

    Update class_funcs.js

--- classes/class_funcs.js
+++ classes/class_funcs.js
@@ -1,33 +1,35 @@
-    // ** Check if element contains specific class
-    var _hasClass = function(el, classValue)
-    {
-        var pattern = new RegExp('(^|\\s)' + classValue + '(\\s|$)');
-        return pattern.test(el.className);  // boolean
-    };
 
-    // ** Add class to element
-    var _addClass = function(el, classValue)
-    {
-        if(!(_hasClass(el, classValue)))
-        {
-            if(el.className === '')
-            {
-                el.className = classValue;
-            }
-            else
-            {
-                el.className += ' ' + classValue;
-            }
-        }
-    };
 
-    // ** Remove class from element
-    var _removeClass = function(el, classValue)
+// ** Check if element contains specific class
+var _hasClass = function(el, classValue)
+{
+    var pattern = new RegExp('(^|\\s)' + classValue + '(\\s|$)');
+    return pattern.test(el.className);  // boolean
+};
+
+// ** Add class to element
+var _addClass = function(el, classValue)
+{
+    if(!(_hasClass(el, classValue)))
     {
-        if(_hasClass(el, classValue))
+        if(el.className === '')
         {
-            var reg = new RegExp('(^|\\s)' + classValue + '(\\s|$)');
-            var newClass = el.className.replace(reg, ' ');
-            el.className = newClass.replace(/^\s+|\s+$/g,''); // remove leading and trailing whitespace
+            el.className = classValue;
         }
-    };
+        else
+        {
+            el.className += ' ' + classValue;
+        }
+    }
+};
+
+// ** Remove class from element
+var _removeClass = function(el, classValue)
+{
+    if(_hasClass(el, classValue))
+    {
+        var reg = new RegExp('(^|\\s)' + classValue + '(\\s|$)');
+        var newClass = el.className.replace(reg, ' ');
+        el.className = newClass.replace(/^\s+|\s+$/g,''); // remove leading and trailing whitespace
+    }
+};
